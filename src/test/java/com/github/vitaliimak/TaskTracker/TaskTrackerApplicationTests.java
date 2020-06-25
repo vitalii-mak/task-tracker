567f63406c2eafeac3ab4ad9cf091478181b765e
Vitalii Makarchenkov <vitalii.mak@gmail.com>
2020-06-25 13:29:07 +0300

    Initial commit

---
+++ src/test/java/com/github/vitaliimak/TaskTracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitaliimak.TaskTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
